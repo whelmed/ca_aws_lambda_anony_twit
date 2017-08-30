629926520246bd1eae30c90599d3926b93d3645e
Ben Lambert <ben.lambert@cloudacademy.com>
2017-08-30 19:31:57 -0400

    add settings to vars in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ zip -u twit.zip main.py
 
 BUCKET=ca-serverless-demo-py
 REGION=us-east-2
+ACCOUNT_ID=425607006828
 
 aws s3 mb s3://$BUCKET --region $REGION || true
 aws s3 cp twit.zip s3://$BUCKET --region $REGION
@@ -19,12 +20,12 @@ aws cloudformation deploy --template-file ./template.yml --stack-name anony-twit
 # https://www.youtube.com/watch?v=H4LM_jw5zzs
 API_GATEWAY=$(aws apigateway get-rest-apis | python -c "import json, sys; s = json.loads(sys.stdin.read()); print([i['id'] for i in s['items'] if i['name'] == 'anony-twit'].pop())")
 
+# This is all temporary until I can figure out how to access the API Gateway ID in the CF template.
 # For each func in $(aws lambda list-functions) where Functions[n].FunctionName like 'anony-twit'
-
 IFS=',' read -r -a array <<< $(aws lambda list-functions | python -c "import json, sys; s = json.loads(sys.stdin.read()); print(','.join([i['FunctionName'] for i in s['Functions'] if i['FunctionName'].startswith('anony-twit')]))")
 
 for i in "${array[@]}"
 do
-   aws lambda add-permission --function-name "$i" --statement-id lambda-anony-twit-$(((RANDOM % 10000) + 1)) --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:us-east-2:425607006828:$API_GATEWAY/*/*/*"
+   aws lambda add-permission --function-name "$i" --statement-id lambda-anony-twit-$(((RANDOM % 10000) + 1)) --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn "arn:aws:execute-api:$REGION:$ACCOUNT_ID:$API_GATEWAY/*/*/*"
 done
 
